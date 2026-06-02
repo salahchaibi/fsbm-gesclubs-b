43210034cad3647f951c439fc8c6a0d290fbdbeb
DOUAAN <naggourdouaa@gmail.com>
2026-06-02 16:05:44 +0200

    fix syntax error PageAccueilController

--- app/Http/Controllers/PageAccueilController.php
+++ app/Http/Controllers/PageAccueilController.php
@@ -26,15 +26,13 @@ class PageAccueilController extends Controller
         try {
             $section = $request->input('section');
             $data = json_decode($request->input('data', '{}'), true) ?? [];
-
             DB::table('page_accueil')->updateOrInsert(
                 ['section' => $section],
                 ['data' => json_encode($data), 'updated_at' => now(), 'created_at' => now()]
             );
-
             return response()->json(['success' => true, 'section' => $section]);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-}" " 
+}
