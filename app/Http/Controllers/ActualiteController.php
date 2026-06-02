3ffbee9c6bac4d45eaa62bc91cc4d3efeb39f658
DOUAAN <naggourdouaa@gmail.com>
2026-06-03 01:09:18 +0200

    switch actualite to imgbb

--- app/Http/Controllers/ActualiteController.php
+++ app/Http/Controllers/ActualiteController.php
@@ -8,25 +8,14 @@ use Illuminate\Support\Facades\Http;
 
 class ActualiteController extends Controller
 {
-    private function uploadToCloudinary($file)
-   {
-    $cloudName = env('CLOUDINARY_CLOUD_NAME');
-    $apiKey = env('CLOUDINARY_API_KEY');
-    $apiSecret = env('CLOUDINARY_API_SECRET');
-    $timestamp = time();
-    $signature = sha1("folder=fsbm/actualites&timestamp={$timestamp}{$apiSecret}");
-
-    $response = Http::attach(
-        'file', file_get_contents($file->getRealPath()), $file->getClientOriginalName()
-    )->post("https://api.cloudinary.com/v1_1/{$cloudName}/image/upload", [
-        'api_key'   => $apiKey,
-        'timestamp' => $timestamp,
-        'signature' => $signature,
-        'folder'    => 'fsbm/actualites',
-    ]);
-
-    \Log::info('Cloudinary response: ' . json_encode($response->json()));
-    return $response->json()['secure_url'] ?? null;
+    private function uploadImage($file)
+    {
+        $response = Http::post('https://api.imgbb.com/1/upload', [
+            'key' => env('IMGBB_API_KEY'),
+            'image' => base64_encode(file_get_contents($file->getRealPath())),
+            'name' => $file->getClientOriginalName(),
+        ]);
+        return $response->json()['data']['url'] ?? null;
     }
 
     public function index()
@@ -38,14 +27,10 @@ class ActualiteController extends Controller
     {
         $request->validate(['titre' => 'required|string']);
         $data = $request->except(['image']);
-        \Log::info('Files reçus: ' . json_encode(array_keys($request->allFiles())));
-        \Log::info('Has image: ' . ($request->hasFile('image') ? 'oui' : 'non'));
-
         if ($request->hasFile('image')) {
-            $url = $this->uploadToCloudinary($request->file('image'));
+            $url = $this->uploadImage($request->file('image'));
             if ($url) $data['image'] = $url;
         }
-
         $actualite = Actualite::create($data);
         return response()->json($actualite->load('club'), 201);
     }
@@ -59,12 +44,10 @@ class ActualiteController extends Controller
     {
         $actualite = Actualite::findOrFail($id);
         $data = $request->except(['image', '_method']);
-
         if ($request->hasFile('image')) {
-            $url = $this->uploadToCloudinary($request->file('image'));
+            $url = $this->uploadImage($request->file('image'));
             if ($url) $data['image'] = $url;
         }
-
         $actualite->update($data);
         return response()->json($actualite->load('club'));
     }
